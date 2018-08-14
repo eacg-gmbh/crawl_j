9832428b620e14e27bf9862abe6b02ac4425facc
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-08-14 13:15:32 +0200

    updated config

---
+++ mvn.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export M2_HOME=/opt/mvn
+export M2=/opt/mvn/bin
+/opt/mvn/bin/mvn -f $1 $2
---
+++ mvn305.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export M2=/opt/apache-maven-3.0.5/bin
+export M2_HOME=/opt/apache-maven-3.0.5
+/opt/apache-maven-3.0.5/bin/mvn -f $1 $2
