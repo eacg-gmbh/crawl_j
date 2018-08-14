6ba8f0ffdba66297e998e345404de17f8b33f8f4
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-08-14 13:29:45 +0200

    updated config

--- mvn.sh
+++ mvn.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+source /mnt/crawl_j/setcreds.sh
+
 export M2_HOME=/opt/mvn
 export M2=/opt/mvn/bin
+export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
 /opt/mvn/bin/mvn -f $1 $2
--- mvn305.sh
+++ mvn305.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+source /mnt/crawl_j/setcreds.sh
+
 export M2=/opt/apache-maven-3.0.5/bin
 export M2_HOME=/opt/apache-maven-3.0.5
+export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 /opt/apache-maven-3.0.5/bin/mvn -f $1 $2
