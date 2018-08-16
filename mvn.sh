762a595b31161857719cf821d889918b75bd081f
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-08-16 16:09:59 +0200

    updated config

--- mvn.sh
+++ mvn.sh
@@ -6,5 +6,12 @@ export M2_HOME=/opt/mvn
 export M2=/opt/mvn/bin
 export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export MC_PORT_11211_TCP_PORT=11211
+export MC_PORT_11211_TCP_ADDR=memcached-service.ve.eacg.intern
+export RM_PORT_5672_TCP_PORT=5672
+export RM_PORT_5672_TCP_ADDR=rabbitmq-service.ve.eacg.intern
+export DB_PORT_27017_TCP_PORT=27017
+export DB_PORT_27017_TCP_ADDR=db1.ve.eacg.intern
+export RAILS_ENV=enterprise
 
 /opt/mvn/bin/mvn -f $1 $2
--- mvn305.sh
+++ mvn305.sh
@@ -6,4 +6,12 @@ export M2=/opt/apache-maven-3.0.5/bin
 export M2_HOME=/opt/apache-maven-3.0.5
 export MAVEN_OPTS=-Djava.net.preferIPv4Stack=true
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export MC_PORT_11211_TCP_PORT=11211
+export MC_PORT_11211_TCP_ADDR=memcached-service.ve.eacg.intern
+export RM_PORT_5672_TCP_PORT=5672
+export RM_PORT_5672_TCP_ADDR=rabbitmq-service.ve.eacg.intern
+export DB_PORT_27017_TCP_PORT=27017
+export DB_PORT_27017_TCP_ADDR=db1.ve.eacg.intern
+export RAILS_ENV=enterprise
+
 /opt/apache-maven-3.0.5/bin/mvn -f $1 $2
